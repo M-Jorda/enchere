95b091b18b58c33354b948e84b01e12c5d73b342
Melchior Gibson <jorda.j.fr@gmail.com>
2024-01-22 08:53:38 +0100

    petit commit

--- src/main/java/fr/eni/encheres/servlet/visitor/ServletHomeVisitor.java
+++ src/main/java/fr/eni/encheres/servlet/visitor/ServletHomeVisitor.java
@@ -7,7 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+ 
 @WebServlet("/Visitor/Home")
 public class ServletHomeVisitor extends HttpServlet {
 	private static final long serialVersionUID = 1L;
